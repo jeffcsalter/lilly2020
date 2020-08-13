43952f0ac890254ce77686f8d76670fc6b016f66
jeffcsalter <68400353+jeffcsalter@users.noreply.github.com>
2020-08-13 16:37:01 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,17 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lilly2020' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lilly2020' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lilly2020' ), 'lilly2020', '<a href="https://jeffcsalter.com">Jeff Salter</a>' );
-				?>
+			Copyright &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>.
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
